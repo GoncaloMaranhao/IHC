d0dccb8fc23143cd8b2ffbad8623160a50ea9b9a
Gonçalo Maranhão <goncalo.rodrigues@ua.pt>
2023-05-13 17:21:25 +0100

    able to navigate page

--- MyApplicationx/app/src/main/java/com/example/myapplicationx/MainActivity.java
+++ MyApplicationx/app/src/main/java/com/example/myapplicationx/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myapplicationx;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.loginbtn);
+        
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Tarefa2_1.class);
+                startActivity(intent);
+            }
+        });
     }
 }

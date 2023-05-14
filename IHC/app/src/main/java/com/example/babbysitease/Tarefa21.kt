d1b551ee5dba5c6f5cbeefedbf7713f8b474bf66
Gonçalo Maranhão <goncalo.rodrigues@ua.pt>
2023-05-14 19:05:25 +0100

    remaking project with kotlin

---
+++ IHC/app/src/main/java/com/example/babbysitease/Tarefa21.kt
@@ -0,0 +1,54 @@
+package com.example.babbysitease
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.babbysitease.databinding.ActivityTarefa21Binding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.TextView
+import java.util.Locale
+
+class Tarefa21 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTarefa21Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityTarefa21Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.apply {
+            title = "Clients"
+            setDisplayHomeAsUpEnabled(true)
+        }
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_tarefa21)
+        navView.setupWithNavController(navController)
+
+        val client1Name = "Ana"
+        val client2Name = "Catarina"
+
+        val firstLetter1: TextView = binding.firstLetter1
+        val firstLetter2: TextView = binding.firstLetter2
+
+        firstLetter1.text = client1Name.first().uppercase(Locale.getDefault())
+        firstLetter2.text = client2Name.first().uppercase(Locale.getDefault())
+
+
+        binding.clientInfoButton.setOnClickListener {
+            val intent = Intent(this, Tarefa22::class.java)
+            startActivity(intent)
+        }
+    }
+
+    // Handle click on the back button in the ActionBar
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+}

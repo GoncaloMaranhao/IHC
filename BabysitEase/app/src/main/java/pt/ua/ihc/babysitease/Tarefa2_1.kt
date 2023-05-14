0c637e0f279155acc1ffd5c13bf1901b9f715d35
Gonçalo Maranhão <goncalo.rodrigues@ua.pt>
2023-05-14 12:11:41 +0100

    fixing duplicate error

---
+++ BabysitEase/app/src/main/java/pt/ua/ihc/babysitease/Tarefa2_1.kt
@@ -0,0 +1,35 @@
+package pt.ua.ihc.babysitease
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import pt.ua.ihc.babysitease.databinding.ActivityTarefa21Binding
+
+class Tarefa2_1 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTarefa21Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityTarefa21Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_tarefa21)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}

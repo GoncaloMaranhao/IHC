ed51bb8192ec4d967333c90df26874f7690f8977
Gonçalo Maranhão <goncalo.rodrigues@ua.pt>
2023-05-29 00:04:13 +0100

    added check to see if user wants to leave before saving information at the end of task2

--- IHC/app/src/main/java/com/example/babbysitease/Tarefa26.kt
+++ IHC/app/src/main/java/com/example/babbysitease/Tarefa26.kt
@@ -2,6 +2,7 @@ package com.example.babbysitease
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -36,42 +37,80 @@ class Tarefa26 : AppCompatActivity() {
       navView.setOnNavigationItemSelectedListener { item ->
         when (item.itemId) {
           R.id.navigation_home -> {
-            val intent = Intent(this, Tarefa32::class.java)
-            startActivity(intent)
-            true
+              AlertDialog.Builder(this)
+                  .setTitle("Unsaved Changes")
+                  .setMessage("You have unsaved information. Are you sure you wish to exit the page?")
+                  .setPositiveButton(android.R.string.yes) { dialog, which ->
+                      val intent = Intent(this, Tarefa32::class.java)
+                      startActivity(intent)
+                  }
+                  .setNegativeButton(android.R.string.no, null)
+                  .setIcon(android.R.drawable.ic_dialog_alert)
+                  .show()
+              true
           }
           R.id.navigation_dashboard -> {
-            navController.navigate(R.id.navigation_dashboard)
-            val intent = Intent(this, Tarefa11::class.java)
-            startActivity(intent)
-            true
+              AlertDialog.Builder(this)
+                  .setTitle("Unsaved Changes")
+                  .setMessage("You have unsaved information. Are you sure you wish to exit the page?")
+                  .setPositiveButton(android.R.string.yes) { dialog, which ->
+                      val intent = Intent(this, Tarefa32::class.java)
+                      startActivity(intent)
+                  }
+                  .setNegativeButton(android.R.string.no, null)
+                  .setIcon(android.R.drawable.ic_dialog_alert)
+                  .show()
+              true
           }
           R.id.navigation_notifications -> {
-            navController.navigate(R.id.navigation_notifications)
-            val intent = Intent(this, Tarefa33::class.java)
-            startActivity(intent)
-            true
+              AlertDialog.Builder(this)
+                  .setTitle("Unsaved Changes")
+                  .setMessage("You have unsaved information. Are you sure you wish to exit the page?")
+                  .setPositiveButton(android.R.string.yes) { dialog, which ->
+                      val intent = Intent(this, Tarefa32::class.java)
+                      startActivity(intent)
+                  }
+                  .setNegativeButton(android.R.string.no, null)
+                  .setIcon(android.R.drawable.ic_dialog_alert)
+                  .show()
+              true
           }
           else -> false
         }
       }
 
       supportActionBar?.apply {
-        title = "Information"
+        title = "Save Changes"
         setDisplayHomeAsUpEnabled(true)
       }
-
-      binding.saveChangesButton.setOnClickListener {
-        val intent = Intent(this, Tarefa32::class.java)
-        startActivity(intent)
-      }
-
+        binding.saveChangesButton.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("Confirm Save")
+                .setMessage("Are you sure you want to save changes?")
+                .setPositiveButton(android.R.string.yes) { dialog, which ->
+                    val intent = Intent(this, Tarefa32::class.java)
+                    startActivity(intent)
+                }
+                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show()
+        }
     }
 
-  override fun onSupportNavigateUp(): Boolean {
-    val intent = Intent(this, Tarefa25::class.java)
-    startActivity(intent)
-    finish()
-    return true
-  }
+    override fun onSupportNavigateUp(): Boolean {
+        AlertDialog.Builder(this)
+            .setTitle("Unsaved Changes")
+            .setMessage("You have unsaved information. Are you sure you wish to exit the page?")
+            .setPositiveButton(android.R.string.yes) { dialog, which ->
+                val intent = Intent(this, Tarefa25::class.java)
+                startActivity(intent)
+                finish()
+            }
+            .setNegativeButton(android.R.string.no, null)
+            .setIcon(android.R.drawable.ic_dialog_alert)
+            .show()
+
+        return true
+    }
+
 }
